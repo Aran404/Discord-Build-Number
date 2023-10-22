b888e3434ee94c431b7c7091a1633083ecc3eb16
Aran <96021763+Aran404@users.noreply.github.com>
2023-10-22 14:26:18 -0400

    Add files via upload

---
+++ client.go
@@ -0,0 +1,30 @@
+package build
+
+import (
+	"fmt"
+	"strings"
+)
+
+func New(proxy string) (*Instance, error) {
+	c := NewOptions()
+	c.SetNewCookieJar()
+	c.SetTimeout(60)
+
+	httpClient, err := c.NewClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.Contains(proxy, "@") {
+		subs := strings.Split(proxy, "@")
+		proxy = fmt.Sprintf("%v:%v", subs[1], subs[0])
+	}
+
+	client := &Client{HttpClient: *httpClient}
+
+	return &Instance{
+		Client: client,
+		Proxy:  proxy,
+	}, nil
+}
---
+++ structs.go
@@ -0,0 +1,40 @@
+package build
+
+import (
+	http "github.com/bogdanfinn/fhttp"
+
+	tls_client "github.com/bogdanfinn/tls-client"
+)
+
+type Instance struct {
+	*Client
+	Proxy       string
+	UserAgent   string
+	BuildNumber string
+}
+
+type Options struct {
+	Settings []tls_client.HttpClientOption
+}
+
+type ClientOptions interface {
+	SetTimeout(int)
+	SetNewCookieJar()
+	SetNotFollowRedirects()
+	SetProxy(string)
+	NewClient() (*tls_client.HttpClient, error)
+}
+
+type Client struct {
+	tls_client.HttpClient
+}
+
+type RequestResponse struct {
+	Error                error
+	StatusCode           int
+	Ok                   bool
+	StatusCodeDefinition string
+	Body                 []byte
+	Json                 map[string]interface{}
+	Request              *http.Response
+}
