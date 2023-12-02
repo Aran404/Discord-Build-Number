ef8a646f9a354b9170c2769537ca045d8433974e
Aran <96021763+Aran404@users.noreply.github.com>
2023-12-01 23:11:21 -0500

    Update build_number.go

--- build_number.go
+++ build_number.go
@@ -53,6 +53,6 @@ func (in *Instance) GetSuperProperties() string {
 		log.Fatal("Build number not initialized")
 	}
 
-	parsed := `{"os":"Windows","browser":"Chrome","device":"","system_locale":"en-US","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36","browser_version":"112.0.0.0","os_version":"10","referrer":"","referring_domain":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_build_number":%s,"client_event_source":null,"design_id":0}`
+	parsed := `{"os":"Windows","browser":"Chrome","device":"","system_locale":"en-US","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36","browser_version":"117.0.0.0","os_version":"10","referrer":"","referring_domain":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_build_number":%s,"client_event_source":null,"design_id":0}`
 	return base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf(parsed, in.BuildNumber)))
 }
